906788cd16534585ea9b9e0a83e41d8d56e30553
tanwirul anhar <mohammad.tanwirul29@gmail.com>
2024-06-02 01:51:45 +0800

    ada perubahan di button navbar login

--- src/component/NavbarLogin.jsx
+++ src/component/NavbarLogin.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 const NavbarLogin = () => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate(); 
-  const token = localStorage.getItem('acces_token');
+
 
 
   useEffect(() => {
@@ -67,69 +67,3 @@ const NavbarLogin = () => {
 
 export default NavbarLogin;
 
-
-// import React, { useEffect, useState } from "react";
-// import image from "../assets/logo.png";
-// import { Link, useNavigate } from "react-router-dom";
-// import axios from "axios";
-
-
-// const NavbarLogin = () => {
-//   const [user, setUser] = useState(null);
-//   const navigate = useNavigate();
-
-
-
-//   useEffect(() => {
-//     axios
-//       .get("https://reqres.in/api/users/4")
-//       .then((response) => {
-//         console.log(response?.data.data);
-//         setUser(response?.data.data);
-
-        
-//       })
-//       .catch((err) => console.log(err));
-//   }, []);
-
-//   const handleClick = () => {
-//     return (
-//       setTimeout(() => {
-//         navigate("/");
-//       }, 2000);
-//     )
-//   }
-
-//   return (
-//     <>
-//       <div className="flex justify-between px-24 py-8 ">
-//         <Link to="/homeLogin">
-//           <div className="w-56 h-12 ">
-//             <img src={image} alt="logo" />
-//           </div>
-//         </Link>
-
-//         <div className="flex gap-4">
-//           <button onClick={handleClick} className="w-24 h-10 mt-2 font-bold text-center text-white bg-cyan-800 rounded-2xl">Log Out</button>
-//           {user && (
-//             <div className="flex gap-4" key={user.id}>
-//               <img
-//                 className="w-12 h-12 rounded-full"
-//                 src={user.avatar}
-//                 alt="img"
-//               />
-//               <div>
-//                 <h1 className="mt-2 font-sans text-sm font-semibold text-white">
-//                   {user.first_name} {user.last_name}
-//                 </h1>
-//                 <p className="text-sm text-white">Welcome Back!</p>
-//               </div>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default NavbarLogin;
